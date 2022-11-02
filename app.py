4695848bb7631bd276067d24204ddaa418fd3463
Juttu <69361530+Juttu@users.noreply.github.com>
2022-11-03 01:58:17 +0530

    a

---
+++ app.py
@@ -0,0 +1,16 @@
+
+import streamlit as st 
+
+f = open ("students.txt", "a")
+
+f.write("a")
+
+f1 = open ("students.txt", "r")
+contents=f1.readlines()
+print(contents)
+
+
+f.close()
+
+st.metric(label="Temperature", value=contents[0], delta="1.2 °F")
+
